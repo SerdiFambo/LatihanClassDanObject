6b53124d96225fa101f07d8a06395525eaf2bbf0
M S I <M S I@MSI>
2023-03-30 17:44:12 +0700

    Add project files.

---
+++ LatihanClassDanObject/Taxi.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassDanObject
+{
+    internal class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Nama Driver : {0}", DriverName);
+            if (OnDuty == true)
+            {
+                Console.WriteLine("Driver Status : YES ", OnDuty);
+            }
+            else
+            {
+                Console.WriteLine("Driver Status : NO ", OnDuty);
+            }
+            Console.WriteLine("Number of Passenger : {0}\n", NumPassenger);
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
+        }   
+            public void DropOffPassenger()
+            {
+                Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+            }
+    }
+}
